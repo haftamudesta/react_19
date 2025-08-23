49b36f3915b5dd7d76d2b6623de4bda3acadbfa2
haftamudesta <desta451616@hotmail.com>
2025-08-23 12:13:03 -0800

    Add initial configuration

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-2xl text-teal-400 text-center mt-3 underline">React 19 Recap</h1>
+    </>
+  )
+}
+
+export default App
