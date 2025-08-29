1f0a5d82d661164666237d68786ccffefc418021
haftamudesta <desta451616@hotmail.com>
2025-08-29 11:20:36 -0800

    Add tasks

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,46 @@
-import React,{useState} from 'react';
+import React,{memo, useState,useCallback} from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
+const TaskList=memo(function TaskList({tasks,onRemove}){
+  return (
+    <>
+    <ul className='mb-4'>
+        {tasks.map((task)=>(
+          <li className='font-bold flex gap-4 mb-4' key={task.id}>{task.text}
+          <button onClick={()=>onRemove(task.id)} className='bg-amber-300 text-red-500 rounded-b-lg px-2 py-0.5'>Remove task</button>
+          </li>
+))}
+      </ul>
+      </>
+  )
+})
 
 function App() {
-  const [task,setTask]=useState([
+  const [tasks,setTasks]=useState([
     {id:1,
       text:"task one"
     },
-    {id:1,
-      text:"task one"
+    {id:2,
+      text:"task two"
     },
   ])
   const [newTask,setNewTask]=useState('')
+  const addTask=()=>{
+    if(newTask.trim()==="") return
+    setTasks((prev)=>[...prev,{id:uuidv4(),text:newTask}])
+    setNewTask("")
+  }
+  const removetask=useCallback((id)=>{
+    setTasks((prev)=>prev.filter(ta=>ta.id!==id))
+  },[])
 
   return (
     <div className='flex justify-center items-center'>
       <div className='flex flex-col gap-3 justify-center'>
       <h1 className='text-2xl text-lime-300 underline mb-4'>Task Manager</h1>
       <input type="text" value={newTask} onChange={(e)=>setNewTask(e.target.value)} className='border-2 border-emerald-400'/>
-      <button className='text-purple-500 bg-gray-400 font-bold'>Add Task</button>
+      <button className='text-purple-500 bg-gray-400 font-bold' onClick={addTask}>Add Task</button>
+      <TaskList tasks={tasks} onRemove={removetask} />
     </div>
     </div>
   )
